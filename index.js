0115c35a36a95e9138f1ff56f3f1892fadbea25e
Patrick Conant <patrick@prcapps.com>
2014-12-22 10:42:54 -0500

    Iniital Commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
+
+app.set('port', (process.env.PORT || 5000));
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
+
 
 app.get('/', function(req, res){
 //  res.send('<h1>Hello world</h1>');
